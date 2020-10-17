24da34b0a1ea37a6217b6c4167cd93dfbd598dab
Anas EL MASSAFI <anas.elmassafi@gmail.com>
2020-10-17 18:58:51 +0100

    first commit

---
+++ src/test/java/com/elmassafi/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmassafi.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
